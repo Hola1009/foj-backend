01bcc361f95dd8f3770c762517b88ffc36dcc62f
hola1009 <fancier_fox@163.com>
2025-01-04 19:51:38 +0800

    # 初始化项目

---
+++ foj-modules/foj-frined/src/main/java/com/fancier/friend/OjFriendApplication.java
@@ -0,0 +1,12 @@
+package com.fancier.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OjFriendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjFriendApplication.class, args);
+    }
+}
