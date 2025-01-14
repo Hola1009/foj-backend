cb5568750e1c59f86294be37814ae44e43468616
hola1009 <fancier_fox@163.com>
2025-01-15 07:45:45 +0800

    # 添加服务端 url 标识常量

---
+++ foj-common/foj-common-core/src/main/java/com/fancier/foj/common/core/constant/HttpConstants.java
@@ -0,0 +1,26 @@
+package com.fancier.foj.common.core.constant;
+
+/**
+ * @author <a href="https://github.com/hola1009">fancier</a>
+ **/
+public interface HttpConstants {
+    /**
+     * 服务端url标识
+     */
+    String SYSTEM_URL_PREFIX = "system";
+
+    /**
+     * 用户端url标识
+     */
+    String FRIEND_URL_PREFIX = "friend";
+
+    /**
+     * 令牌自定义标识
+     */
+    String AUTHENTICATION = "Authorization";
+
+    /**
+     * 令牌前缀
+     */
+    String PREFIX = "Bearer ";
+}
