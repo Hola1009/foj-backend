9b6142249f199ae148e2fad2792a3cf2a47877b9
hola1009 <fancier_fox@163.com>
2025-01-15 07:43:06 +0800

    # 为 JwtUtils 添加解析 payload 的方法

--- foj-common/foj-common-security/src/main/java/com/fancier/foj/common/security/utils/JwtUtils.java
+++ foj-common/foj-common-security/src/main/java/com/fancier/foj/common/security/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package com.fancier.foj.common.security.utils;
 
+import com.fancier.foj.common.core.constant.JwtConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -32,5 +33,16 @@ public class JwtUtils {
      */
     public static Claims parseToken(String token, String secret ) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    public static String getUserId(Claims claims) {
+        return toStr(claims.get(JwtConstants.LOGIN_USER_ID));
+    }
+
+    private static String toStr(Object value) {
+        if (value == null) {
+            return "";
+        }
+        return value.toString();
     }
 }
