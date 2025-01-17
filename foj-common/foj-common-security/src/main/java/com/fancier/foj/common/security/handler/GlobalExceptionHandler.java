3100a4b41d443c40d44ce980beaa2ea71bfe04c0
hola1009 <fancier_fox@163.com>
2025-01-17 09:33:34 +0800

    # 添加全局`BindException`异常处理方法

--- foj-common/foj-common-security/src/main/java/com/fancier/foj/common/security/handler/GlobalExceptionHandler.java
+++ foj-common/foj-common-security/src/main/java/com/fancier/foj/common/security/handler/GlobalExceptionHandler.java
@@ -1,13 +1,23 @@
 package com.fancier.foj.common.security.handler;
 
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.fancier.foj.common.core.constant.enums.ResultCode;
 import com.fancier.foj.common.core.domain.vo.Result;
 import com.fancier.foj.common.security.exception.BusinessException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理器
@@ -53,5 +63,21 @@ public class GlobalExceptionHandler {
         String requestURI = request.getRequestURI();
         log.error("请求地址'{}',发⽣异常.", requestURI, e);
         return Result.failure();
+    }
+
+    @ExceptionHandler(BindException.class)
+    public Result handleBindException(BindException e) {
+        log .error(e.getMessage());
+        String message = join(e.getAllErrors(), DefaultMessageSourceResolvable::getDefaultMessage);
+        return Result.failure(ResultCode.FAILED_PARAMS_VALIDATE.getCode(), message);
+    }
+    private <E> String join(Collection<E> collection, Function<E, String> function) {
+        if (CollUtil. isEmpty (collection)) {
+            return StrUtil. EMPTY ;
+        }
+        return collection.stream()
+                .map(function)
+                .filter(Objects:: nonNull )
+                .collect(Collectors.joining (", "));
     }
 }
