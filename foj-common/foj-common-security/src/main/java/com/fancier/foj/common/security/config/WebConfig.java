d8b3d02306bbe62fb888ec97a7f00fddf96ccd6d
hola1009 <fancier_fox@163.com>
2025-01-24 11:11:37 +0800

    # 添加 WebConfig 配置及导入类 解决 LocalDateTime 序列化问题

---
+++ foj-common/foj-common-security/src/main/java/com/fancier/foj/common/security/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.fancier.foj.common.security.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+/**
+ * @author <a href="https://github.com/hola1009">fancier</a>
+ **/
+@Configuration
+public class WebConfig {
+     @Bean
+     public Jackson2ObjectMapperBuilderCustomizer customizeLocalDateTimeFormat() {
+         return jacksonObjectMapperBuilder -> {
+             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+             LocalDateTimeDeserializer deserializer = new LocalDateTimeDeserializer(formatter);
+             LocalDateTimeSerializer serializer = new LocalDateTimeSerializer(formatter);
+             jacksonObjectMapperBuilder.serializerByType(LocalDateTime.class, serializer);
+             jacksonObjectMapperBuilder.deserializerByType(LocalDateTime.class, deserializer);
+         };
+     }
+}
