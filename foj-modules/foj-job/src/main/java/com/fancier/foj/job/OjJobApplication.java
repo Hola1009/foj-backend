25b3e6447d80395aeb6efe3db74d1a95e8c928d2
hola1009 <fancier_fox@163.com>
2025-02-03 15:03:01 +0800

    添加MapperScan注解优化MyBatis配置

--- foj-modules/foj-job/src/main/java/com/fancier/foj/job/OjJobApplication.java
+++ foj-modules/foj-job/src/main/java/com/fancier/foj/job/OjJobApplication.java
@@ -1,10 +1,12 @@
 package com.fancier.foj.job;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.fancier.foj.**.mapper")
 public class OjJobApplication {
     public static void main(String[] args) {
         SpringApplication.run(OjJobApplication.class, args);
